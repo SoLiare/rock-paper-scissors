01e57e4f837450dd296a5217f116eeb6ff4125c2
SoLiare <rawissara.r@ku.th>
2017-12-17 23:54:32 +0700

    Random buttons for player

--- core/src/com/mygdx/game/Condition.java
+++ core/src/com/mygdx/game/Condition.java
@@ -1,25 +1,30 @@
 package com.mygdx.game;
 
+import java.util.Random;
+
 public class Condition {
 	int cases = 2;
-	int button = 0;
+	int buttonNum = 0;
 	
 	public Condition (int player, int bot) {
-		if (bot == player) {
+		if(bot == player) {
 			cases = 0;
-			button = 2;
+			buttonNum = 2;
 		}
 		else {
-			if (player - bot == 1 || player - bot == -2) {
+			if(player - bot == 1 || player - bot == -2) {
 				cases = 1;
-				button = 1;
+				buttonNum = 1;
 			}
 			else {
 				cases = -1;
-				button = 3;
+				buttonNum = 3;
 			}
 		}
 	}
 	
-	
+	public int random () {
+		Random rand = new Random();
+		return rand.nextInt(4);
+	}
 }
--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -2,14 +2,15 @@ package com.mygdx.game;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
 	private RPSGame rpsGame;
 	private Condition condition;
-	
 	private Texture bg;
 	private Texture rPlayer;
 	private Texture ppPlayer;
@@ -17,13 +18,21 @@ public class GameScreen extends ScreenAdapter {
 	private Texture rBot;
 	private Texture ppBot;
 	private Texture ssBot;
+	private Texture w;
+	private Texture s;
+	private Texture a;
+	private Texture d;
 	
 	boolean newRound = true;
 	int bot = 0;
 	
+	boolean win = false;
+	int goal = 10;
+	int mash = 0;
+	int button = 4;
+	
 	public GameScreen (RPSGame rpsGame) {
 		this.rpsGame = rpsGame;
-		
 		bg = new Texture("bg.png");
 		rPlayer = new Texture("rPlayer.png");
 		ppPlayer = new Texture("ppPlayer.png");
@@ -31,45 +40,96 @@ public class GameScreen extends ScreenAdapter {
 		rBot = new Texture("rBot.png");
 		ppBot = new Texture("ppBot.png");
 		ssBot = new Texture("ssBot.png");
+		w = new Texture("wButton.png");
+		s = new Texture("sButton.png");
+		a = new Texture("aButton.png");
+		d = new Texture("dButton.png");
 	}
 	
-	public void render(float delta) {
+	public void render (float delta) {
 		SpriteBatch batch = rpsGame.batch;
 		batch.begin();
 		batch.draw(bg, 0, 0);
 		
-		if (rpsGame.card == 1) {
+		if(rpsGame.card == 1) {
 			batch.draw(rPlayer, 300, 50);
 		}
-		else if (rpsGame.card == 2) {
+		else if(rpsGame.card == 2) {
 			batch.draw(ppPlayer, 300, 50);
 		}
-		else if (rpsGame.card == 3) {
+		else if(rpsGame.card == 3) {
 			batch.draw(ssPlayer, 300, 50);
 		}
 		
-		if (newRound) {
+		if(newRound) {
 			bot = random();
 		}
-		if (bot == 1) {
+		if(bot == 1) {
 			batch.draw(rBot, 300, 320);
 		}
-		else if (bot == 2) {
+		else if(bot == 2) {
 			batch.draw(ppBot, 300, 320);
 		}
-		else if (bot == 3) {
+		else if(bot == 3) {
 			batch.draw(ssBot, 300, 320);
 		}
+		
+		if(newRound) {
+			condition = new Condition(rpsGame.card, bot);
+		}
+		
+		if(condition.buttonNum > 0) {
+			if(button == 4) {
+				button = condition.random();
+			}
+			
+			if(button == 0) {
+				batch.draw(w, 50, 250);
+
+				if(Gdx.input.isKeyJustPressed(Keys.W)) {
+					mash = mash + 1;
+				}
+			}
+			else if(button == 1) {
+				batch.draw(s, 50, 250);
+				
+				if(Gdx.input.isKeyJustPressed(Keys.S)) {
+					mash = mash + 1;
+				}
+			}
+			else if(button == 2) {
+				batch.draw(a, 50, 250);
+				
+				if(Gdx.input.isKeyJustPressed(Keys.A)) {
+					mash = mash + 1;
+				}
+			}
+			else if(button == 3) {
+				batch.draw(d, 50, 250);
+				
+				if(Gdx.input.isKeyJustPressed(Keys.D)) {
+					mash = mash + 1;
+				}
+			}
+			
+			if(goal == mash) {
+				win = true;
+			}
+			
+			if(win) {
+				mash = 0;
+				condition.buttonNum = condition.buttonNum - 1;
+				button = 4;
+				win = false;
+			}
+		}
 		newRound = false;
+		
 		batch.end();
-		
-		condition = new Condition(rpsGame.card, bot);
-		
 	}
 	
-	public int random () {
+	public int random() {
 		Random rand = new Random();
-		
 		return rand.nextInt(3) + 1;
 	}
 }
